0b631ab90f0cea486925c520258c21b0ead5b17d
iamvineettiwari <iamvineettiwari012@gmail.com>
2023-12-24 12:08:11 +0530

    Added support for EXISTS/DEL/INCR/DECR

--- cmd/bin/main.go
+++ cmd/bin/main.go
@@ -13,6 +13,10 @@ func main() {
 	handler.AddHandler("ECHO", handler.Echo)
 	handler.AddHandler("SET", handler.Set)
 	handler.AddHandler("GET", handler.Get)
+	handler.AddHandler("EXISTS", handler.Exists)
+	handler.AddHandler("DEL", handler.Delete)
+	handler.AddHandler("INCR", handler.Incr)
+	handler.AddHandler("DECR", handler.Decr)
 
 	redisServer.Start()
 }
